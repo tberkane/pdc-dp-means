6147437d6109bb03b0d141e7b021d120f9530e5d
Thomas Berkane <thomas.berkane@epfl.ch>
2024-08-16 00:35:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,17 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
-import os
 
-def pyload(name):
-    ns = {}
-    with open(name, encoding="utf-8") as f:
-        exec(compile(f.read(), name, "exec"), ns)
-    return ns
-
-repo_root = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(repo_root, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-ns = pyload(os.path.join(repo_root, "pdc_dp_means", "release.py"))
-version = ns["__version__"]
-
-# Define the extension
-extensions = [
-    Extension(
-        "pdc_dp_means.dp_means_cython",
-        sources=['pdc_dp_means/dp_means_cython.pyx'],
-        include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-    )
-]
+def get_extensions():
+    extensions = [
+        Extension(
+            "pdc_dp_means.dp_means_cython",
+            ["pdc_dp_means/dp_means_cython.pyx"],
+            include_dirs=[numpy.get_include()],
+        )
+    ]
+    return extensions
 
 setup(
     name="pdc-dp-means",
@@ -34,10 +21,6 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="BSD3",
-    packages=find_packages(),
-    package_data={
-        "pdc_dp_means": ["*.so", "*.pyd", "docs/*"],
-    },
     keywords="dp-means clustering",
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -47,21 +30,11 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    install_requires=[
-        "scikit-learn>=1.2,<1.3",
-        "numpy>=1.23.0,<2.0",
-        "Cython>=0.29.0",
-    ],
     tests_require=[
         "pytest",
         "scikit-learn",
         "numpy",
     ],
-    ext_modules=cythonize(
-        extensions,
-        compiler_directives={'language_level': "3"},
-        include_path=[numpy.get_include()]
-    ),
     include_package_data=True,
     zip_safe=False,
     project_urls={
@@ -69,4 +42,18 @@ setup(
         "Tracker": "https://github.com/BGU-CS-VIL/pdc-dp-means",
         "Documentation": "https://pdc-dp-means.readthedocs.io/en/latest/",
     },
+    packages=find_packages(),
+    package_data={
+        "pdc-dp-means": ["docs/*"],
+    },
+    install_requires=[
+        "scikit-learn>=1.2,<1.3",
+        "numpy>=1.23.0,<2.0",
+        "Cython>=0.29.0",
+    ],
+    setup_requires=[
+        "Cython>=0.29.0",
+        "numpy>=1.23.0,<2.0",
+    ],
+    ext_modules=cythonize(get_extensions()),
 )
